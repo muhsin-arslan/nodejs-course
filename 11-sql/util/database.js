9244500e5e0c377af1b1927d5e0cba8526585578
Muhsin ARSLAN <devmuhsinarslan@gmail.com>
2021-04-02 14:31:19 +0300

    Update

---
+++ 11-sql/util/database.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("node_complete", "root", {
+  dialect: "mysql",
+  host: "localhost",
+  operatorsAliases: false,
+});
+
+module.exports = sequelize;
